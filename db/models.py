f1bad353f90ae6b5b37e5d9170b769a101d97c5b
cookies0606 <kism9923@gmail.com>
2025-06-10 15:08:44 +0900

    Update models.py

--- db/models.py
+++ db/models.py
@@ -4,3 +4,10 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Employee(Base):
+    __tablename__ = 'employees'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    department = Column(String, nullable=False)
+    hire_date = Column(Date, nullable=False)
+    phone = Column(String)
